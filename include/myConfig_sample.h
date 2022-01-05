6695d43a5f2370e9be9943364e94c948463e60fc
Coturex <coturex@gmail.com>
2022-01-05 15:26:24 +0100

    add deprecated hardware PZEM V2

--- include/myConfig_sample.h
+++ include/myConfig_sample.h
@@ -1,12 +1,2 @@
-const char* ssid = "";
-const char* password = "";
-
-const char* mqtt_server = "test.mosquitto.org";
-const char* topic_prefix = "pzem" ;
-
-
-// Unique idx of module, which is used to build the domoticz MQTT message on which data are published on Domoticz/in topic
-#define DOMO_TOPIC "domoticz/in" 
-#define PZEM1_IDX1 888  // domoticz power idx 
-#define PZEM1_IDX2 999  // domoticz voltage idx
+const char* ota_password = "123";
 
